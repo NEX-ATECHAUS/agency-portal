eb85fd1e0873ffadcb79a31baf7f81af6ca4797f
NEX-ATECHAUS <hello@nex-a.com.au>
2026-05-01 16:54:18 +1000

    Update _sheets-auth.js

--- api/_sheets-auth.js
+++ api/_sheets-auth.js
@@ -1,4 +1,3 @@
-// Shared Google auth helper using OAuth2 (no service account key needed)
 const { google } = require('googleapis');
 
 function getOAuthClient() {
@@ -13,9 +12,34 @@ function getOAuthClient() {
   return oauth2Client;
 }
 
+function getServiceAccountAuth() {
+  const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
+  if (credentials.private_key) {
+    credentials.private_key = credentials.private_key.replace(/\\n/g, '\n');
+  }
+  return new google.auth.GoogleAuth({
+    credentials,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+}
+
 function getSheetsClient() {
-  const auth = getOAuthClient();
+  const hasOAuthConfig = !!(
+    process.env.GMAIL_CLIENT_ID &&
+    process.env.GMAIL_CLIENT_SECRET &&
+    process.env.GMAIL_REFRESH_TOKEN
+  );
+
+  let auth;
+  if (hasOAuthConfig) {
+    auth = getOAuthClient();
+  } else if (process.env.GOOGLE_SERVICE_ACCOUNT) {
+    auth = getServiceAccountAuth();
+  } else {
+    throw new Error('Missing Sheets auth. Set Gmail OAuth env vars or GOOGLE_SERVICE_ACCOUNT.');
+  }
+
   return google.sheets({ version: 'v4', auth });
 }
 
-module.exports = { getOAuthClient, getSheetsClient };
+module.exports = { getOAuthClient, getServiceAccountAuth, getSheetsClient };
